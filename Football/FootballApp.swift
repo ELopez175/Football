532c1dd90d892b6d8fb4365ada24b3f198382a79
ELopez175 <eibilopez1088@gmail.com>
2025-11-10 11:21:43 -0800

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by LOPEZ, EIBI on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FootballTests/FootballTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTests.swift
+//  FootballTests
+//
+//  Created by LOPEZ, EIBI on 11/4/25.
+//
+
+import Testing
+@testable import Football
+
+struct FootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
